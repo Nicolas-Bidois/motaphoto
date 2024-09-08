8fb2c0b261ea8499b036c72f5060cebd9700781b
Bidois Nicolas <nicolasbidois2@gmail.com>
2024-09-08 16:08:53 +0200

    finalisation

--- js/scripts.js
+++ js/scripts.js
@@ -86,135 +86,178 @@ document.addEventListener("DOMContentLoaded", function() {
 /////////////////////////////////////////////////////////
 
 
-
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("Le DOM est chargé.");
+    var categoryFilter = document.getElementById('category-filter');
+    var formatFilter = document.getElementById('format-filter');
+    var orderFilter = document.getElementById('order-filter');
 
+    // Vérifier si les éléments existent avant d'ajouter les événements
+    if (categoryFilter && formatFilter && orderFilter) {
+        categoryFilter.addEventListener('change', applyFilters);
+        formatFilter.addEventListener('change', applyFilters);
+        orderFilter.addEventListener('change', applyFilters);
+    } else {
+        console.error("Un ou plusieurs filtres sont manquants.");
+    }
 
-    // Récupérer tous les éléments ayant la classe "expand-icon"
-    var expandIcons = document.querySelectorAll('.expand-icon');
-    console.log("Nombre d'icônes d'agrandissement trouvées : " + expandIcons.length);
-    
-    // Parcourir tous les éléments et ajouter un gestionnaire d'événement au clic
-    expandIcons.forEach(function(expandIcon) {
-        console.log("Ajout d'un gestionnaire d'événement au clic pour une icône d'agrandissement.");
-    
-        expandIcon.addEventListener('click', function() {
-            console.log("Clic détecté sur une icône d'agrandissement.");
-    
-            // Récupérer l'URL de l'image à partir de l'attribut "src" de l'élément parent
-            var imgElement = this.closest('.photo-ajust').querySelector('.photo-apparenté, .photo-apparenté2');
-            if (imgElement) {
-                var imageUrl = imgElement.getAttribute('src');
-                console.log("URL de l'image : " + imageUrl);
-    
-                // Créer un élément d'image pour la lightbox
-                var lightboxImg = document.createElement('img');
-                lightboxImg.src = imageUrl;
-    
-                // Créer un conteneur pour la lightbox
-                var lightboxContainer = document.createElement('div');
-                lightboxContainer.classList.add('lightbox-container');
-                lightboxContainer.appendChild(lightboxImg);
-    
-                // Créer les éléments pour la référence et le terme
-                var referenceParagraph = document.createElement('p');
-                referenceParagraph.classList.add('lightbox__reference');
-                lightboxContainer.appendChild(referenceParagraph);
-    
-                var termParagraph = document.createElement('p');
-                termParagraph.classList.add('lightbox__term');
-                lightboxContainer.appendChild(termParagraph);
-    
-                // Ajouter les boutons de fermeture, suivant et précédent
-                var closeBtn = document.createElement('button');
-                closeBtn.classList.add('lightbox__close');
-                closeBtn.textContent = 'X';
-                lightboxContainer.appendChild(closeBtn);
-    
-                var nextBtn = document.createElement('button');
-                nextBtn.classList.add('lightbox__next');
-                nextBtn.innerHTML = 'Suivant <i class="fa-solid fa-arrow-right"></i>';
-                lightboxContainer.appendChild(nextBtn);
-    
-                var prevBtn = document.createElement('button');
-                prevBtn.classList.add('lightbox__prev');
-                prevBtn.innerHTML = '<i class="fa-solid fa-arrow-left"></i> Précédent';
-                lightboxContainer.appendChild(prevBtn);
-    
-                // Liste des URL des images, en excluant les images avec la classe 'hero_head'
-                var imageUrls = Array.from(document.querySelectorAll('.photo-ajust .photo-apparenté:not(.hero_head), .photo-ajust .photo-apparenté2:not(.hero_head)'))
-                    .map(img => img.getAttribute('src'));
-    
-                // Variable pour suivre l'index de l'image actuellement affichée
-                var currentImageIndex = imageUrls.indexOf(imageUrl);
-    
-                // Fonction pour mettre à jour les éléments de référence et de terme
-                function updateReferenceAndTerm() {
-                    var currentIcon = Array.from(expandIcons).find(icon => {
-                        var img = icon.closest('.photo-ajust').querySelector('.photo-apparenté, .photo-apparenté2');
-                        return img && img.getAttribute('src') === imageUrls[currentImageIndex];
-                    });
-                    if (currentIcon) {
-                        var referenceElement = currentIcon.closest('.photo-ajust').querySelector('.reference');
-                        var termElement = currentIcon.closest('.photo-ajust').querySelector('.term');
-                        if (referenceElement && termElement) {
-                            console.log("Référence trouvée : " + referenceElement.textContent);
-                            console.log("Terme trouvé : " + termElement.textContent);
-                            referenceParagraph.textContent = referenceElement.textContent;
-                            termParagraph.textContent = termElement.textContent;
-                        } else {
-                            console.error("Les éléments de référence et de terme n'ont pas été trouvés.");
-                        }
-                    } else {
-                        console.error("L'élément currentIcon n'a pas été trouvé.");
-                    }
-                }
-    
-                // Mettre à jour les éléments de référence et de terme initialement
-                updateReferenceAndTerm();
-    
-                // Ajouter un événement pour fermer la lightbox au clic sur le bouton de fermeture
-                closeBtn.addEventListener('click', function() {
-                    document.body.removeChild(lightboxContainer);
-                    console.log("Lightbox supprimée de la page.");
-                });
-    
-                // Logique pour afficher l'image suivante
-                function showNextImage() {
-                    currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
-                    lightboxImg.src = imageUrls[currentImageIndex];
-                    // Mettre à jour les éléments de référence et de terme
-                    updateReferenceAndTerm();
-                }
-    
-                // Logique pour afficher l'image précédente
-                function showPreviousImage() {
-                    currentImageIndex = (currentImageIndex - 1 + imageUrls.length) % imageUrls.length;
-                    lightboxImg.src = imageUrls[currentImageIndex];
-                    // Mettre à jour les éléments de référence et de terme
-                    updateReferenceAndTerm();
-                }
-    
-                // Ajouter un gestionnaire d'événement pour le bouton suivant
-                nextBtn.addEventListener('click', showNextImage);
-    
-                // Ajouter un gestionnaire d'événement pour le bouton précédent
-                prevBtn.addEventListener('click', showPreviousImage);
-    
-                // Ajouter la lightbox à la page
-                document.body.appendChild(lightboxContainer);
-                console.log("Lightbox ajoutée à la page.");
-    
+    // Fonction pour appliquer les filtres et mettre à jour les photos
+    function applyFilters() {
+        var category_id = categoryFilter.value;
+        var format_id = formatFilter.value;
+        var order = orderFilter.value;
+
+        var data = {
+            'action': 'filter_photos', // Action côté serveur
+            'type': 'combined', // Type de requête pour combiner les filtres
+            'category_id': category_id,
+            'format_id': format_id,
+            'order': order // Paramètre pour l'ordre
+        };
+
+        // Requête AJAX pour appliquer les filtres
+        jQuery.post("/wp-admin/admin-ajax.php", data, function(response) {
+            var photosContainer = document.querySelector('.photo-index');
+            if (photosContainer) {
+                photosContainer.innerHTML = response; // Mettre à jour les photos avec le résultat de la requête
+                console.log("Photos mises à jour avec les filtres appliqués.");
+                // Réinitialiser les événements de la lightbox après la mise à jour des images
+                initLightbox();
             } else {
-                console.error("Aucun élément img trouvé avec la classe .photo-apparenté ou .photo-apparenté2.");
+                console.error("Élément avec la classe 'photo-index' introuvable.");
             }
-    
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.error("Erreur AJAX : " + textStatus, errorThrown);
         });
-    });
+    }
+
+    // Fonction pour initialiser la lightbox
+    function initLightbox() {
+        // Récupérer tous les éléments ayant la classe "expand-icon"
+        var expandIcons = document.querySelectorAll('.expand-icon');
+        console.log("Nombre d'icônes d'agrandissement trouvées : " + expandIcons.length);
+
+        // Parcourir tous les éléments et ajouter un gestionnaire d'événement au clic
+        expandIcons.forEach(function(expandIcon) {
+            console.log("Ajout d'un gestionnaire d'événement au clic pour une icône d'agrandissement.");
+    
+            expandIcon.addEventListener('click', function() {
+                console.log("Clic détecté sur une icône d'agrandissement.");
+    
+                // Récupérer l'URL de l'image à partir de l'attribut "src" de l'élément parent
+                var imgElement = this.closest('.photo-ajust').querySelector('.photo-apparenté, .photo-apparenté2');
+                if (imgElement) {
+                    var imageUrl = imgElement.getAttribute('src');
+                    console.log("URL de l'image : " + imageUrl);
+    
+                    // Créer un élément d'image pour la lightbox
+                    var lightboxImg = document.createElement('img');
+                    lightboxImg.src = imageUrl;
+    
+                    // Créer un conteneur pour la lightbox
+                    var lightboxContainer = document.createElement('div');
+                    lightboxContainer.classList.add('lightbox-container');
+                    lightboxContainer.appendChild(lightboxImg);
+    
+                    // Créer les éléments pour la référence et le terme
+                    var referenceParagraph = document.createElement('p');
+                    referenceParagraph.classList.add('lightbox__reference');
+                    lightboxContainer.appendChild(referenceParagraph);
+    
+                    var termParagraph = document.createElement('p');
+                    termParagraph.classList.add('lightbox__term');
+                    lightboxContainer.appendChild(termParagraph);
+    
+                    // Ajouter les boutons de fermeture, suivant et précédent
+                    var closeBtn = document.createElement('button');
+                    closeBtn.classList.add('lightbox__close');
+                    closeBtn.textContent = 'X';
+                    lightboxContainer.appendChild(closeBtn);
+    
+                    var nextBtn = document.createElement('button');
+                    nextBtn.classList.add('lightbox__next');
+                    nextBtn.innerHTML = 'Suivant <i class="fa-solid fa-arrow-right"></i>';
+                    lightboxContainer.appendChild(nextBtn);
+    
+                    var prevBtn = document.createElement('button');
+                    prevBtn.classList.add('lightbox__prev');
+                    prevBtn.innerHTML = '<i class="fa-solid fa-arrow-left"></i> Précédent';
+                    lightboxContainer.appendChild(prevBtn);
+    
+                    // Liste des URL des images, en excluant les images avec la classe 'hero_head'
+                    var imageUrls = Array.from(document.querySelectorAll('.photo-ajust .photo-apparenté:not(.hero_head), .photo-ajust .photo-apparenté2:not(.hero_head)'))
+                        .map(img => img.getAttribute('src'));
+    
+                    // Variable pour suivre l'index de l'image actuellement affichée
+                    var currentImageIndex = imageUrls.indexOf(imageUrl);
+    
+                    // Fonction pour mettre à jour les éléments de référence et de terme
+                    function updateReferenceAndTerm() {
+                        var currentIcon = Array.from(expandIcons).find(icon => {
+                            var img = icon.closest('.photo-ajust').querySelector('.photo-apparenté, .photo-apparenté2');
+                            return img && img.getAttribute('src') === imageUrls[currentImageIndex];
+                        });
+                        if (currentIcon) {
+                            var referenceElement = currentIcon.closest('.photo-ajust').querySelector('.reference');
+                            var termElement = currentIcon.closest('.photo-ajust').querySelector('.term');
+                            if (referenceElement && termElement) {
+                                console.log("Référence trouvée : " + referenceElement.textContent);
+                                console.log("Terme trouvé : " + termElement.textContent);
+                                referenceParagraph.textContent = referenceElement.textContent;
+                                termParagraph.textContent = termElement.textContent;
+                            } else {
+                                console.error("Les éléments de référence et de terme n'ont pas été trouvés.");
+                            }
+                        } else {
+                            console.error("L'élément currentIcon n'a pas été trouvé.");
+                        }
+                    }
+    
+                    // Mettre à jour les éléments de référence et de terme initialement
+                    updateReferenceAndTerm();
+    
+                    // Ajouter un événement pour fermer la lightbox au clic sur le bouton de fermeture
+                    closeBtn.addEventListener('click', function() {
+                        document.body.removeChild(lightboxContainer);
+                        console.log("Lightbox supprimée de la page.");
+                    });
+    
+                    // Logique pour afficher l'image suivante
+                    function showNextImage() {
+                        currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
+                        lightboxImg.src = imageUrls[currentImageIndex];
+                        // Mettre à jour les éléments de référence et de terme
+                        updateReferenceAndTerm();
+                    }
+    
+                    // Logique pour afficher l'image précédente
+                    function showPreviousImage() {
+                        currentImageIndex = (currentImageIndex - 1 + imageUrls.length) % imageUrls.length;
+                        lightboxImg.src = imageUrls[currentImageIndex];
+                        // Mettre à jour les éléments de référence et de terme
+                        updateReferenceAndTerm();
+                    }
+    
+                    // Ajouter un gestionnaire d'événement pour le bouton suivant
+                    nextBtn.addEventListener('click', showNextImage);
+    
+                    // Ajouter un gestionnaire d'événement pour le bouton précédent
+                    prevBtn.addEventListener('click', showPreviousImage);
+    
+                    // Ajouter la lightbox à la page
+                    document.body.appendChild(lightboxContainer);
+                    console.log("Lightbox ajoutée à la page.");
+    
+                } else {
+                    console.error("Aucun élément img trouvé avec la classe .photo-apparenté ou .photo-apparenté2.");
+                }
+    
+            });
+        });
+    }
+
+    // Initialiser la lightbox au chargement de la page
+    initLightbox();
 });
-
-
 
 
 
@@ -325,43 +368,6 @@ jQuery(document).ready(function($) {
 
 
 
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var categoryFilter = document.getElementById('category-filter');
-    var formatFilter = document.getElementById('format-filter');
-    var orderFilter = document.getElementById('order-filter');
-
-    categoryFilter.addEventListener('change', applyFilters);
-    formatFilter.addEventListener('change', applyFilters);
-    orderFilter.addEventListener('change', applyFilters);
-
-    function applyFilters() {
-        var category_id = categoryFilter.value;
-        var format_id = formatFilter.value;
-        var order = orderFilter.value;
-
-        var data = {
-            'action': 'filter_photos', // Action pour identifier le traitement côté serveur
-            'type': 'combined', // Type de requête pour afficher les images en fonction des filtres sélectionnés
-            'category_id': category_id,
-            'format_id': format_id,
-            'order': order // Ajout du paramètre d'ordre
-        };
-
-        // Envoyer la requête AJAX au serveur
-        jQuery.post("/wp-admin/admin-ajax.php", data, function(response) {
-            var photosContainer = document.querySelector('.photo-index');
-            if (photosContainer) {
-                photosContainer.innerHTML = response; // Mettre à jour le contenu avec les nouvelles photos
-            } else {
-                console.error("Element with class 'photo-index' not found.");
-            }
-        });
-    }
-});
 
 
 
